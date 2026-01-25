71c25d50e81839280e82df09ed08a1da5ec64b78
ChekhovskyiVlad <v.chek1318@gmail.com>
2026-01-25 12:50:07 +0100

    25.01.26(1) commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const http = require("http");
+const PORT = 3000
+
+const server = http.createServer((req, res) => {
+    res.write("hello");
+    res.end();
+});
+
+server.listen(PORT, () => {
+    console.log("server is running...")
+    console.log()
+})
